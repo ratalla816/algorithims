3b9c651c7cbd598f304e54e02364e4bb3811b691
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-12-05 12:21:13 -0600

    solved fizz-buzz

--- fizz-buzz/Unsolved/fizz-buzz.js
+++ fizz-buzz/Unsolved/fizz-buzz.js
@@ -6,21 +6,39 @@
 
 // var fizzBuzz = function(arr) {};
 
-function fizzBuzz() {
-  for (let i = 1; i <= 100; i++) {
-    // MODULUS OPERATOR % //
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzBuzz');
-    } else if (i % 3 === 0) {
+// This solution is correct but fails tests //
+// function fizzBuzz() {
+//   for (let i = 1; i <= 100; i++) {
+//     // MODULUS OPERATOR % //
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizz Buzz');
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+
+// const output = fizzBuzz();
+
+// console.log(output);
+
+// This solution is correct and passes all tests //
+var fizzBuzz = function(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var currentNumber = arr[i];
+
+    if (currentNumber % 15 === 0) {
+      console.log("Fizz Buzz");
+    } else if (currentNumber % 3 === 0) {
       console.log("Fizz");
-    } else if (i % 5 === 0) {
+    } else if (currentNumber % 5 === 0) {
       console.log("Buzz");
     } else {
-      console.log(i);
+      console.log(currentNumber);
     }
   }
-}
-
-const output = fizzBuzz();
-
-console.log(output);
+};
